ed3be6d1e969c13db52e84aacabf3d7bc0e746df
Yuya Tsuda <yytsuda@gmail.com>
2016-12-07 08:41:23 +0900

    update init.el
    再起動すると emacs の起動時にエラーになった。
    path 設定を init.el に移したほうが良い？と思ったのだが、
    そうでも無さそうなので、やめる。
    結局 `cask install` で治ったのだが。
    
    以前 emacs 25 にしようとした時にも起きた症状で、
    気になる。理由を知りたい。
    Cask が load-path に入らないとか？

--- init.el
+++ init.el
@@ -4,8 +4,10 @@
 ;; You may delete these explanatory comments.
 (package-initialize)
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; configure load-path
 ;; EMACS 25 に移行する時に必要か？
-;; ;; load-path (path.el から移行)
+;; load-path (path.el から移行)
 ;; (defun add-to-load-path (&rest paths)
 ;;   "recursively, add specified directory to load-path"
 ;;   (let (path)
@@ -27,22 +29,8 @@
 (cask-initialize)
 
 ;; init-loader
+;; Cask で導入している
 (require 'init-loader)
 (init-loader-load "~/.emacs.d/conf")
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   (quote
-    ("a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" "a802c77b818597cc90e10d56e5b66945c57776f036482a033866f5f506257bca" default)))
- '(wc-modeline-format "[%tlL/%tcC]"))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(col-highlight ((t (:background "gray20"))))
- '(hl-line ((t (:background "gray20")))))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
