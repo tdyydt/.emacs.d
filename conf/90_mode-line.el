0657718094263d2fddb69bd3d6de2aac1600af32
Yuya Tsuda <yytsuda@gmail.com>
2016-12-21 15:54:45 +0900

    poweline => sml (smart mode line)
    smart mode line にした
    起動時に、なんか言われる。
    emacs config の書き方が悪いんだとか書いてあるが。

--- conf/90_mode-line.el
+++ conf/90_mode-line.el
@@ -49,33 +49,71 @@
 
 (add-hook 'after-change-major-mode-hook 'clean-mode-line)
 
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; smart modeline
+;; via: https://github.com/Malabarba/smart-mode-line#features
+
+;; setup
+(sml/setup)
+
+;; because Emacs recommend this to me...
+(setq sml/theme 'light)
+
+;; avoid messages
+;; (setq sml/no-confirm-load-theme t)
+
+;;;; Pathname abbreviations
+;; もとから定義されていたのを上書きする場合は t を付けてはいけない。
+;; (add-to-list 'sml/replacer-regexp-list '("^~/Git-Projects/" ":Git:") t)
+(add-to-list 'sml/replacer-regexp-list '("^~/Documents/Code/" ":Code:"))
+(add-to-list 'sml/replacer-regexp-list '("^~/Documents/KU/" ":KU:"))
+;; ED => Emacs
+(add-to-list 'sml/replacer-regexp-list '("^~/\\.emacs\\.d/" ":Emacs:"))
+(add-to-list 'sml/replacer-regexp-list '("^~/Dropbox/" ":DBox:"))
+
+
+;;;; pathname のところの設定
+(setq sml/shorten-directory t)
+;; pathname の幅
+;; (MIN-WIDTH . MAX-WIDTH) で指定するとのこと
+;; 最小なし (nil), 最大 44 を意図する。
+(setq sml/name-width '(nil . 44))
+
+;;;; mode 名のところの設定
+(setq sml/shorten-modes t)
+;; あまり意味が分からん。mode 名のところが長くなりすぎないようにしたい。
+;; 行数とかは左に出てるので、現状どうでもいい。
+(setq sml/mode-width 'right)
+
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; powerline
 ;; via: http://emacs.stackexchange.com/questions/281/how-do-i-get-a-fancier-mode-line-that-uses-solid-colors-and-triangles
 ;; via: http://blog.shibayu36.org/entry/2014/04/01/094543
 ;; via: http://blog.shibayu36.org/entry/2014/02/02/192924
 
-;; なくても良さげ
-;; (require 'smart-mode-line)
+;; ;; なくても良さげ
+;; ;; (require 'smart-mode-line)
 
-;; powerline には
-;; いくつか変種があるとのこと
-;; via: https://www.emacswiki.org/emacs/PowerLine
+;; ;; powerline には
+;; ;; いくつか変種があるとのこと
+;; ;; via: https://www.emacswiki.org/emacs/PowerLine
 
-;; setup powerline
-(require 'powerline)
+;; ;; setup powerline
+;; (require 'powerline)
 
-;; powerline-themes に定義されている
-(powerline-default-theme)
-;(powerline-center-theme)
+;; ;; powerline-themes に定義されている
+;; (powerline-default-theme)
+;; ;(powerline-center-theme)
 
-;; set separator:
-;; nil, bar, curve, ...
-;; あまり綺麗に表示されない
-;; via: http://ytsk.hatenablog.com/entry/2015/09/23/021856
-(setq powerline-default-separator 'curve)
-;; ファイルのどの位置にいるかの、示すバーを表示するか
-;;(setq powerline-display-hud t)
+;; ;; set separator:
+;; ;; nil, bar, curve, ...
+;; ;; あまり綺麗に表示されない
+;; ;; via: http://ytsk.hatenablog.com/entry/2015/09/23/021856
+;; (setq powerline-default-separator 'curve)
+;; ;; ファイルのどの位置にいるかの、示すバーを表示するか
+;; ;;(setq powerline-display-hud t)
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -84,36 +122,36 @@
 ;; color name list
 ;; via: http://raebear.net/comp/emacscolors.html
 
-;; for active window:
-(progn
-  (set-face-attribute 'mode-line nil
-                      :foreground "Brack"
-                      :background "violet"
-                      :box nil)
-  ;; major-mode, minor-mode のところ
-  (set-face-attribute 'powerline-active1 nil
-                      :foreground "Brack"
-                      :background "White"
-                      :inherit 'mode-line)
-  ;; 時計とかのところ
-  (set-face-attribute 'powerline-active2 nil
-                      :foreground "Brack"
-                      ;; :background "chocolate"
-                      :background "DarkOrange"
-                      :inherit 'mode-line))
+;; ;; for active window:
+;; (progn
+;;   (set-face-attribute 'mode-line nil
+;;                       :foreground "Brack"
+;;                       :background "violet"
+;;                       :box nil)
+;;   ;; major-mode, minor-mode のところ
+;;   (set-face-attribute 'powerline-active1 nil
+;;                       :foreground "Brack"
+;;                       :background "White"
+;;                       :inherit 'mode-line)
+;;   ;; 時計とかのところ
+;;   (set-face-attribute 'powerline-active2 nil
+;;                       :foreground "Brack"
+;;                       ;; :background "chocolate"
+;;                       :background "DarkOrange"
+;;                       :inherit 'mode-line))
 
-;; for inactive windows:
-(let ((bg-color "gray20")
-      (fg-color "White"))
-  (set-face-attribute 'mode-line-inactive nil
-                      :foreground fg-color
-                      :background bg-color
-                      :box nil)
-  (set-face-attribute 'powerline-inactive1 nil
-                      :foreground fg-color
-                      :background bg-color
-                      :inherit 'mode-line)
-  (set-face-attribute 'powerline-inactive2 nil
-                      :foreground fg-color
-                      :background bg-color
-                      :inherit 'mode-line))
+;; ;; for inactive windows:
+;; (let ((bg-color "gray20")
+;;       (fg-color "White"))
+;;   (set-face-attribute 'mode-line-inactive nil
+;;                       :foreground fg-color
+;;                       :background bg-color
+;;                       :box nil)
+;;   (set-face-attribute 'powerline-inactive1 nil
+;;                       :foreground fg-color
+;;                       :background bg-color
+;;                       :inherit 'mode-line)
+;;   (set-face-attribute 'powerline-inactive2 nil
+;;                       :foreground fg-color
+;;                       :background bg-color
+;;                       :inherit 'mode-line))
--- init.el
+++ init.el
@@ -34,3 +34,19 @@
 (init-loader-load "~/.emacs.d/conf")
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("84d2f9eeb3f82d619ca4bfffe5f157282f4779732f48a5ac1484d94d5ff5b279" "c74e83f8aa4c78a121b52146eadb792c9facc5b1f02c917e3dbb454fca931223" "3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" "a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" default)))
+ '(wc-modeline-format "[%tlL/%tcC]"))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(col-highlight ((t (:background "gray20"))))
+ '(hl-line ((t (:background "gray20")))))
