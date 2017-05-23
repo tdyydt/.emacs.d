ff138623b38175a3a95f3c61e05a2464ea8550b2
Yuya Tsuda <yytsuda@gmail.com>
2017-05-23 14:22:44 +0900

    ocaml indent setting (tuareg)

--- conf/30_ocaml.el
+++ conf/30_ocaml.el
@@ -13,3 +13,8 @@
 ;; via: https://www.emacswiki.org/emacs/AutoModeAlist
 (add-to-list 'auto-mode-alist
              '("\\.ml[iylp]?\\'" . tuareg-mode))
+
+
+;; customize function indentation
+;; via: https://github.com/ocaml/tuareg#customization
+(setq tuareg-indent-align-with-first-arg nil)
--- conf/30_wc-mode.el
+++ conf/30_wc-mode.el
@@ -1,7 +1,10 @@
 (require 'wc-mode)
 ;; LaTeX ではなくて、すべて小文字らしい
 ;; describe variable するとハッキリする。
-(add-hook 'latex-mode-hook 'wc-mode)
+;; (add-hook 'latex-mode-hook 'wc-mode)
+
+;; wc -m に対応していないようなので、日本語文章ではなかなか
+;; 使用しづらい
 
 ;; (defcustom wc-modeline-format "WC[%W%w/%tw]"
 ;; の設定を。初期値が "WC[%W%w/%tw]" ということである
