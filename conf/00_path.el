49d82fcdb3ddcbf869d000353a42afc94ccfa124
Yuya Tsuda <yytsuda@gmail.com>
2017-06-10 10:48:01 +0900

    Coq settings & update .gitignore

--- conf/00_path.el
+++ conf/00_path.el
@@ -1,15 +1,3 @@
-;; load-path
-(defun add-to-load-path (&rest paths)
-  "recursively, add specified directory to load-path"
-  (let (path)
-    (dolist (path paths paths)
-      (let ((default-directory
-	      (expand-file-name (concat user-emacs-directory path))))
-	(add-to-list 'load-path default-directory)
-	(if (fboundp 'normal-top-level-add-subdirs-to-load-path)
-	    (normal-top-level-add-subdirs-to-load-path))))))
-(add-to-load-path "elisp")
-
 ;; exec-path-from-shell
 ;; emacs の PATH を shell と共有する
 (el-get-bundle exec-path-from-shell)
--- conf/30_coq.el
+++ conf/30_coq.el
@@ -1,34 +1,21 @@
 ;; computing and logic
 
 ;; proof-general
-;; (el-get-bundle proof-general
-;;   :type github
-;;   :pkgname "ProofGeneral/PG"
-;;   )
-;; (el-get-bundle ProofGeneral/PG)
+;;(el-get-bundle proof-general)
 
-;; TODO: imaxima 同様もう少しマシな書き方があるか
-;; (load "/usr/local/Cellar/proof-general/4.4/share/emacs/site-lisp/proof-general/site-start.d/pg-init.el")
-;; Cellar を探して、init らしきものをロード
-;; /usr/local/share/emacs/site-lisp/proof-general
-;; に symlink が張られていて、load-path にはいる
+;; el-git による導入は上手くいかないので、一旦諦める。
+;; github に書いてある方法に従う。
+;; via: https://github.com/ProofGeneral/PG
+(load "~/.emacs.d/lisp/PG/generic/proof-site")
 
 ;; needed
 (setq coq-prog-name "~/.opam/system/bin/coqtop")
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; plugin
 ;; company-coq
 ;;(el-get-bundle elpa:company-coq)
-;; (el-get-bundle
-;;   company-coq
-;;   :type git
-;;   :url "https://github.com/cpitclaudel/company-coq")
 
-
-;; => 依存関係で proof-general がインストールされ、いつもエラーになる
-;; のでコメントアウトする。
-;; proof-general を el-get で入れる選択肢自体は十分あり得る。
+;; install by elpa (melpa)
 
 (add-hook 'coq-mode-hook #'company-coq-mode)
