cb166eb4494f5ca8190d788a0d4db265904d88a6
Yuya Tsuda <yytsuda@gmail.com>
2018-04-13 01:12:03 +0900

    Use copl-mode

--- conf/01_path.el
+++ conf/01_path.el
@@ -1,3 +1,17 @@
 ;; exec-path-from-shell
 ;; emacs の PATH を shell のと共有する
 (exec-path-from-shell-initialize)
+
+;; load-path
+(defun add-to-load-path (&rest paths)
+  "recursively, add specified directory to load-path"
+  (let (path)
+    (dolist (path paths paths)
+      (let ((default-directory
+	      (expand-file-name (concat user-emacs-directory path))))
+	(add-to-list 'load-path default-directory)
+	(if (fboundp 'normal-top-level-add-subdirs-to-load-path)
+	    (normal-top-level-add-subdirs-to-load-path))))))
+(add-to-load-path "elisp")
+
+;;(add-to-list 'load-path "~/.emacs.d/elisp/copl-mode" t)
---
+++ conf/40_copl.el
@@ -0,0 +1,3 @@
+;; プログラミング言語の基礎概念
+;; via: https://github.com/chiro/copl-mode
+(require 'copl-mode)
