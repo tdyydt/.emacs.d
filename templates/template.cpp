4f1529aa66c671b9d18edec1d5e30d7fa18bbd6f
Yuya Tsuda <yytsuda@gmail.com>
2018-06-10 12:54:43 +0900

    Update cpp template

--- templates/template.cpp
+++ templates/template.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
+#include <cassert>
+#include <algorithm>
 #include <string>
 #include <vector>
+#include <tuple>
 #define REP(i,k,n) for(int(i)=(k);(i)<(n);++(i))
 using namespace std;
 
