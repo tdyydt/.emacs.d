356fed60a7a8d83edacfcaa87c0ec0098c63ed18
Yuya Tsuda <yytsuda@gmail.com>
2018-08-29 00:32:06 +0900

    Update web-mode

--- conf/40_web.el
+++ conf/40_web.el
@@ -8,15 +8,14 @@
 
 (add-to-list 'auto-mode-alist '("\\.js[x]?$" . web-mode))
 
+;; edit any js in jsx mode; maybe not good idea?
 (setq web-mode-content-types-alist
       '(("jsx" . "\\.js[x]?\\'")))
-
-;; TODO: 拡張子 .js でもJSX編集モードに
-;; しているのは，若干過激なのかも
 
 (defun my-web-mode-hook ()
   "Hooks for Web mode."
   (setq web-mode-markup-indent-offset 2)
   (setq web-mode-code-indent-offset 2)  ; e.g. js
+  (setq web-mode-enable-auto-indentation nil)
 )
 (add-hook 'web-mode-hook  'my-web-mode-hook)
