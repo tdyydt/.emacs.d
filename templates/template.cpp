9a6889ae61116a541e86177efb6edbe48873b3d9
Yuya Tsuda <yytsuda@gmail.com>
2018-08-29 00:32:23 +0900

    Update cpp template

--- templates/template.cpp
+++ templates/template.cpp
@@ -1,11 +1,8 @@
-#include <iostream>
-#include <cassert>
-#include <algorithm>
-#include <string>
-#include <vector>
-#include <tuple>
+#include <bits/stdc++.h>
 #define REP(i,k,n) for(int(i)=(k);(i)<(n);++(i))
+#define ALL(x) (x).begin(),(x).end()
 using namespace std;
+typedef long long ll;
 
 int main()
 {
