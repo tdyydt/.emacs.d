d7a37a30b76a824085722217d91413ea36186740
Yuya Tsuda <yytsuda@gmail.com>
2018-12-21 14:32:29 +0900

    Make exec-path-from-shell use zsh (for linux)

--- conf/linux-config.el
+++ conf/linux-config.el
@@ -1,2 +1,4 @@
-;; exec-path-from-shell will see zsh
-(setenv "SHELL" "/bin/zsh")
+;; shell-file-name is /bin/bash
+(setq exec-path-from-shell-shell-name "/bin/zsh")
+;; This command will be run for the second time. (no problem?)
+(exec-path-from-shell-initialize)
