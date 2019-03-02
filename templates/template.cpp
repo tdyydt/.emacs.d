10c7f659f4b23f8d44bbc3c6e6398f5f9ec09926
Yuya Tsuda <yytsuda@gmail.com>
2019-03-02 23:15:09 +0900

    Modify template.cpp

--- templates/template.cpp
+++ templates/template.cpp
@@ -1,6 +1,8 @@
 #include <bits/stdc++.h>
 #define REP(i,k,n) for(int(i)=(k);(i)<(n);++(i))
 #define ALL(x) (x).begin(),(x).end()
+#define chmin(x,v) x = min(x, v)
+#define chmax(x,v) x = max(x, v)
 using namespace std;
 typedef long long ll;
 
