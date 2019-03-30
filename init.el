43d3b410e8b361099aeb708dc42ba69735fbe14f
Yuya Tsuda <yytsuda@gmail.com>
2019-03-30 23:53:51 +0900

    Set custom-file; Refactor init.el

--- init.el
+++ init.el
@@ -1,6 +1,7 @@
 ;; packege.el
 ;; (>= emacs-major-version 24)
 (require 'package)
+
 ;; default?
 ;; (add-to-list 'package-archives '("gnu" . "https://elpa.gnu.org/packages/"))
 ;; t ==> add ELEMENT at the end
@@ -10,10 +11,12 @@
              '("marmalade" . "https://marmalade-repo.org/packages/") t)
 (package-initialize)
 
-;; Update/Refresh packages (necessary)
+;; Automatically update the list of packages, only if there is no package list already
 ;; via: https://stackoverflow.com/questions/14836958/updating-packages-in-emacs
 (when (not package-archive-contents)
   (package-refresh-contents))
+
+;;;;;;
 
 ;; Suggestion: require package
 ;; via: https://qiita.com/regashia/items/057b682dd29fbbdadd52
@@ -22,32 +25,18 @@
   (unless (package-installed-p package)
     (package-install package)))
 
-;; init-loader だけ特別扱いして，最初にインストール
-;; 他は，00_package.el でインストール
+;; Other packages than init-loader
+;; are installed by loading conf/00_package.el
 
 ;; init-loader
 (package-install-if-needed 'init-loader)
 (require 'init-loader)
 (init-loader-load "~/.emacs.d/conf")
 
-;; This is the end of my setting part.
-;; Following configs may be the configs added by other programs.
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(agda2-highlight-face-groups (quote default-faces))
- '(custom-safe-themes
-   (quote
-    ("3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" "a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" default)))
- '(package-selected-packages (quote (company-coq async shut-up company)))
- '(wc-modeline-format "[%tlL/%tcC]"))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(col-highlight ((t (:background "gray20"))))
- '(hl-line ((t (:background "gray20")))))
+;; custom-set-variables are written to custom-file
+;; via: http://extra-vision.blogspot.com/2016/10/emacs25-package-selected-packages.html
+(setq custom-file (expand-file-name "custom.el" user-emacs-directory))
+(when (file-exists-p custom-file)
+  (load custom-file))
+
+;; ** END **
